7200adc6f864063ee36f2b98f16df26a7326e96c
Kaibin <diaokaibin@gmail.com>
2020-06-30 20:43:57 +0800

    update

--- src/main/java/chapter03/array/Solution_move_zeroes.java
+++ src/main/java/chapter03/array/Solution_move_zeroes.java
@@ -7,7 +7,17 @@ package chapter03.array;
 public class Solution_move_zeroes {
 
     public void moveZeroes(int[] nums){
+        int j =0;
+        for(int i=0;i<nums.length;i++){
+            if (nums[i] != 0){
 
+                nums[j]= nums[i];
+                if(i!=j){
+                    nums[i]=0;
+                }
+                j++;
+            }
+        }
 
     }
 
