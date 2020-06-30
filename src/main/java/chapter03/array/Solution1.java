6af779d1b5bab500c13203bb746b2cb749e64aa1
Kaibin <diaokaibin@gmail.com>
2020-06-30 19:59:37 +0800

    两数之和

---
+++ src/main/java/chapter03/array/Solution1.java
@@ -0,0 +1,31 @@
+package chapter03.array;
+
+import java.util.HashMap;
+
+/**
+ * 两数之和
+ * https://leetcode-cn.com/problems/two-sum/
+ *
+ */
+public class Solution1 {
+
+
+    public int[] twoSum(int[] nums,int target){
+        int[] res = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = target-nums[i];
+            if (map.containsKey(nums[i])) {
+                res[0] = map.get(nums[i]);
+                res[1] = i;
+            } else {
+                map.put(sum,i);
+            }
+
+        }
+        return res;
+
+    }
+
+}
